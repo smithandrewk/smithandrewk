ff4676c08f7af30ce884f33abc933b832822cd68
andrew-k-smith <andrewsmith1025@gmail.com>
2019-07-31 20:59:45 -0400

    temporary about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import img from './5.JPG';
+import './About.css';
 
 export default class About extends Component {
     render() {
         return (
-            <div>
+            <div id="aboutBody">
                 <h1>This page is under construction.</h1>
+                <img src={img} alt="img" id="img"/>
             </div>
         )
     }
