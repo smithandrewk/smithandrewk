0f3612ec944d29c9087956f5468d0df6b6f09d5c
andrew-k-smith <andrewsmith1025@gmail.com>
2019-07-31 21:00:22 -0400

    numbers api component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import a from './a';
+import './Projects.css';
 
 export default class Projects extends Component {
+    componentDidMount(){
+        a.a(document)
+    }
     render() {
         return (
-            <div>
-                <h1>This page is also under construction.</h1>
+            <div id="projectsBody">
+                <h1>What's your favorite number?</h1>
+                <p id="fact-container"></p>
+                <div className="form">
+                    <input id="a" type="text" name="name" required autoComplete="off"/>
+                    <label htmlFor="name" className="label-name">
+                        <span className="content-name">Number</span>
+                    </label>
+                </div>
             </div>
         )
     }
