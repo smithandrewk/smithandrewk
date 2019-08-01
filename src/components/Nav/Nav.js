5ebfe440f7cf20ca727905357274aedbf5d07041
andrew-k-smith <andrewsmith1025@gmail.com>
2019-07-31 21:00:01 -0400

    updated temporary nav name

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -15,7 +15,7 @@ export default class Nav extends Component {
                 <ul className="nav-links">
                     <li><a href="/">Home</a></li>
                     <li><a href="about">About</a></li>
-                    <li><a href="projects">Projects</a></li>
+                    <li><a href="projects">Fun Fact</a></li>
                     <li><a href="thoughts">Thoughts</a></li>
                     <li><a href="contact">Contact</a></li>
                 </ul>
