5ca1d68463615061c8d72d990684d84aba70a51b
andrew-k-smith <andrewsmith1025@gmail.com>
2019-08-02 09:20:35 -0400

    resize listener + querySelect on mount

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react'
 import navAnim from './navAnim';
+import './Nav.css';
+
 
 
 export default class Nav extends Component {
     componentDidMount(){
-        navAnim();
+        const nav = document.querySelector('.nav-links');
+        const burger = document.querySelector('.hamburger');
+        const navLinks = document.querySelectorAll('.nav-links li');
+        let headline;
+        if(document.querySelector('.headline'))
+            headline = document.querySelector('.headline');
+
+        navAnim(burger, nav, navLinks, headline);
+
+        window.addEventListener('resize', (e) => {
+            if(e.target.innerWidth>768 && nav.classList.contains('nav-active')){
+                nav.classList.toggle('nav-active');
+                if(document.querySelector('.headline'))
+                    headline.classList.toggle('headlineActive');
+                
+                //Animate links
+                navLinks.forEach((link, index) => {
+                    if (link.style.animation) {
+                        link.style.animation = '';
+                    } else {
+                        link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+.5}s`
+                    }
+                });
+                //Burger Animation
+                burger.classList.toggle('toggle');
+            }
+        });
     }
     render() {
         return (
             <nav>
-                <div className="logo">
-                    {/* <h1>Contact</h1> */}
-                </div>
+                <div className="logo"></div>
                 <ul className="nav-links">
                     <li><a href="/">Home</a></li>
-                    <li><a href="about">About</a></li>
-                    <li><a href="projects">Fun Fact</a></li>
+                    <li><a href="about">Gallery</a></li>
+                    <li><a href="projects">Projects</a></li>
                     <li><a href="thoughts">Thoughts</a></li>
                     <li><a href="contact">Contact</a></li>
                 </ul>
