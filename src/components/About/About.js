1d368683b85e69f4e711842bbd0577c0bef5ef39
andrew-k-smith <andrewsmith1025@gmail.com>
2019-08-02 09:19:46 -0400

    add images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
-import img from './5.JPG';
+import img from './img/5.JPG';
+import img2 from './img/tay59.JPG';
+import img3 from './img/tay71.JPG';
+import img4 from './img/tay93.JPG';
 import './About.css';
 
 export default class About extends Component {
     render() {
         return (
             <div id="aboutBody">
-                <h1>This page is under construction.</h1>
-                <img src={img} alt="img" id="img"/>
+                <div className="images">
+                    <div className="image-container fade a">
+                        <img src={img} alt="wyoming" />
+                    </div>
+                    <div className="image-container fade b ">
+                        <img src={img2} alt="keys" />
+                    </div>
+                    <div className="image-container fade c">
+                        <img src={img3} alt="montana" />
+                    </div>
+                    <div className="image-container fade d">
+                        <img src={img4} alt="charlotte" />
+                    </div>
+                </div>
+
             </div>
         )
     }
