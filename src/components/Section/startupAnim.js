cb0572552e9ddd7765ac230e4b8c7cd6b3171ee2
andrew-k-smith <andrewsmith1025@gmail.com>
2019-08-02 09:22:22 -0400

    async

--- src/components/Section/startupAnim.js
+++ src/components/Section/startupAnim.js
@@ -4,52 +4,59 @@ import {
 } from 'gsap/TweenMax';
 
 const startupAnim = () => {
-    const hero = document.querySelector('.hero');
-    const slider = document.querySelector('.slider');
-    const logo = document.querySelector('.logo');
-    const hamburger = document.querySelector('.hamburger');
-    const headline = document.querySelector('.headline');
+    return new Promise((resolve, reject) => {
+        const hero = document.querySelector('.hero');
+        const slider = document.querySelector('.slider');
+        const logo = document.querySelector('.logo');
+        const hamburger = document.querySelector('.hamburger');
+        const headline = document.querySelector('.headline');
+    
+        const tl = new TimelineMax({
+            onComplete: () => {
+                resolve();
+            }
+        });
+    
+        tl.fromTo(hero, 2, {
+                height: "0%"
+            }, {
+                height: "95%",
+                ease: Power2.easeInOut
+            })
+            .fromTo(hero, 1.2, {
+                width: "100%"
+            }, {
+                width: "90%",
+                ease: Power2.easeInOut
+            })
+            .fromTo(slider, 1.2, {
+                opacity: "0"
+            }, {
+                opacity: "1"
+            }, "-=1.2")
+            .fromTo(logo, 0.5, {
+                opacity: 0,
+                x: 30
+            }, {
+                opacity: 1,
+                x: 0
+            }, "-=0.5")
+            .fromTo(hamburger, 0.5, {
+                opacity: 0,
+                x: 30
+            }, {
+                opacity: 1,
+                x: 0
+            }, "-=1")
+            .fromTo(headline, 0.5, {
+                opacity: 0,
+                x: 30
+            }, {
+                opacity: 1,
+                x: 0
+            }, "-=1")
+    })
 
-    const tl = new TimelineMax();
-
-    tl.fromTo(hero, 2, {
-            height: "0%"
-        }, {
-            height: "95%",
-            ease: Power2.easeInOut
-        })
-        .fromTo(hero, 1.2, {
-            width: "100%"
-        }, {
-            width: "90%",
-            ease: Power2.easeInOut
-        })
-        .fromTo(slider, 1.2, {
-            opacity: "0"
-        }, {
-            opacity: "1"
-        }, "-=1.2")
-        .fromTo(logo, 0.5, {
-            opacity: 0,
-            x: 30
-        }, {
-            opacity: 1,
-            x: 0
-        }, "-=0.5")
-        .fromTo(hamburger, 0.5, {
-            opacity: 0,
-            x: 30
-        }, {
-            opacity: 1,
-            x: 0
-        }, "-=1")
-        .fromTo(headline, 0.5, {
-            opacity: 0,
-            x: 30
-        }, {
-            opacity: 1,
-            x: 0
-        }, "-=1")
 }
 
 export default startupAnim;
