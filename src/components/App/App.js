9d2dfe1b783caa18008fe2cd0f0497e7229642b4
andrew-k-smith <andrewsmith1025@gmail.com>
2019-08-02 09:20:14 -0400

    prettify

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,26 +8,22 @@ import About from '../About/About';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
-
 class App extends React.Component {
-  componentDidMount(){
-
-  }
   render(){
     return (
-      <Router>
-        <div className="App">
+      <div className="App">
+        <Router>
           <Nav/>
-            <Switch>
-              <Route path="/" exact component={Section}/>
-              <Route path="/contact" component={Contact}/>
-              <Route path="/thoughts" component={Thoughts}/>
-              <Route path="/projects" component={Projects}/>
-              <Route path="/about" component={About}/>
-            </Switch>
+          <Switch>
+            <Route path="/" exact component={Section}/>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/thoughts" component={Thoughts}/>
+            <Route path="/projects" component={Projects}/>
+            <Route path="/about" component={About}/>
+          </Switch>
           <div className="slider"></div>
-        </div>
-      </Router>
+        </Router>
+      </div>
     );
   }
 }
